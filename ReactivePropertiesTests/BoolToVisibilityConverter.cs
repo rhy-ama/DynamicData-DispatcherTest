9b2250019367ed9a1f1b95e039795fc81f1847f2
rhy-ama <a.malotchko@gmail.com>
2019-12-17 14:37:28 +0100

    Added ReactiveUI properties tests

---
+++ ReactivePropertiesTests/BoolToVisibilityConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace ReactivePropertiesTests
+{
+    /*    // https://reactiveui.net/docs/getting-started/
+        // If we would like to do value conversion using Binding markup extension,
+        // we need to implement the IValueConverter interface.
+        public class BoolToVisibilityConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                // We need to convert True value to Visibility.Visible and False value to
+                // Visibility.Collapsed. Then we need to declare the converter as a static resource.
+                return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                // Our app doesn't need to be able to convert values back, so we won't implement this.
+                return Binding.DoNothing;
+            }
+        }*/
+
+    // https://reactiveui.net/docs/getting-started/
+    // If we would like to do value conversion using Binding markup extension,
+    // we need to implement the IValueConverter interface.
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ ReactivePropertiesTests/TestViewModel.cs
@@ -0,0 +1,58 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactivePropertiesTests
+{
+    public class TestViewModel : ReactiveObject
+    {
+        private string _input1;
+        private string _input2;
+        private bool _switchInputs = false;
+        
+        private readonly ObservableAsPropertyHelper<bool> _isInput1Visible;
+        private readonly ObservableAsPropertyHelper<bool> _isInput2Visible;
+
+
+        public bool SwitchInputs
+        {
+            get => _switchInputs;
+            set => this.RaiseAndSetIfChanged(ref _switchInputs, value);
+        }
+
+        public string Input1Text
+        {
+            get => _input1;
+            set => this.RaiseAndSetIfChanged(ref _input1, value);
+        }
+
+        public string Input2Text
+        {
+            get => _input2;
+            set => this.RaiseAndSetIfChanged(ref _input2, value);
+        }
+
+        public bool IsInput1Visible => _isInput1Visible.Value;
+        public bool IsInput2Visible => _isInput2Visible.Value;
+
+        public TestViewModel()
+        {
+            _isInput1Visible = this
+                .WhenAnyValue(x => x.SwitchInputs)
+                .Select(v => v)
+                .ObserveOnDispatcher()
+                .ToProperty(this, x => x.IsInput1Visible);
+
+            _isInput2Visible = this
+                .WhenAnyValue(x => x.SwitchInputs)
+                .Select(v => !v)
+                .ObserveOnDispatcher()
+                .ToProperty(this, x => x.IsInput2Visible);
+        }
+    }
+
+}
